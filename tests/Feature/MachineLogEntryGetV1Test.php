66ab988be6fc13e85222aa5835f901e0b778b364
akuikialie <aku.4l13@gmail.com>
2026-02-18 11:28:11 +0700

    [unit-test] : initial all test default

---
+++ tests/Feature/MachineLogEntryGetV1Test.php
@@ -0,0 +1,13 @@
+<?php
+
+use App\Models\Shift;
+use App\Models\User;
+use App\Models\UserShift;
+use Database\Seeders\DatabaseSeeder;
+use Laravel\Sanctum\Sanctum;
+
+test('machine_v1_default_route', function () {
+});
+
+test('machine_v1_get', function () {
+});
---
+++ tests/Feature/UserV1Test.php
@@ -0,0 +1,10 @@
+<?php
+
+use App\Models\Shift;
+use App\Models\User;
+use App\Models\UserShift;
+use Database\Seeders\DatabaseSeeder;
+use Laravel\Sanctum\Sanctum;
+
+test('user_v1_default_route', function () {
+});
