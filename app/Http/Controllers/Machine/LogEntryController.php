1adc6bab5f5841ca871063ef40266c3cdb1447d3
Sebastianus Sembara <sembara9090@gmail.com>
2026-02-18 17:11:50 +0800

    fix: logentry controller

--- app/Http/Controllers/Machine/LogEntryController.php
+++ app/Http/Controllers/Machine/LogEntryController.php
@@ -5,11 +5,11 @@ namespace App\Http\Controllers\Machine;
 use App\DTOs\MachineLogDto;
 use App\Enums\MachineLog\EventEnum;
 use App\Http\Controllers\Controller;
-use App\Http\Requests\Machine\IndexLogEntryRequest;
 use App\Http\Requests\Machine\StoreLogEntryRequest;
 use App\Http\Resources\Machine\MachineLogResource;
 use App\Services\MachineLogService;
 use App\Services\UserShiftService;
+use Illuminate\Http\Request;
 use Illuminate\Validation\Rule;
 
 class LogEntryController extends Controller
@@ -22,23 +22,17 @@ class LogEntryController extends Controller
      *
      * @tag Machine Log Entries
      */
-    public function index(IndexLogEntryRequest $request)
+    public function index(Request $request)
     {
-        $validated = $request->validate([
-            'limit' => ['nullable', 'integer', 'min:1', 'max:100'],
-            'filter.event' => ['nullable', 'string', Rule::in(['login_success', 'login_failed', 'start_work', 'end_work', 'machine_error', 'maintenance'])],
-            'filter.date_from' => ['nullable', 'date'],
-            'filter.date_to' => ['nullable', 'date', 'after_or_equal:filter.date_from'],
-        ]);
-
         $user = $request->user();
 
         // Get active user shift
         $userShift = UserShiftService::getActiveUserShift($user);
 
         $logs = MachineLogService::getMachineLogs(
-            userShift: $userShift,
             limit: $request->query('limit', 15),
+            userId: $userShift->user_id,
+            machineCode: $userShift->machine_code
         );
 
         return MachineLogResource::collection($logs);
