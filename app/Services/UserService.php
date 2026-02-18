b21ec456adc4a2484e69dd48e32ffdb66287d705
Sebastianus Sembara <sembara9090@gmail.com>
2026-02-18 09:47:57 +0800

    fix backoffice/user: modified ilike to like for search feature

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -31,8 +31,8 @@ class UserService
         // Apply search if provided
         if (!empty($search)) {
             $query->where(function ($q) use ($search) {
-                $q->where('name', 'ILIKE', "%{$search}%")
-                  ->orWhere('employee_number', 'ILIKE', "%{$search}%");
+                $q->where('name', 'LIKE', "%{$search}%")
+                  ->orWhere('employee_number', 'LIKE', "%{$search}%");
             });
         }
 
