87342f9ef05ee27942fdaafca7e155e081c0b430
Audhy <audhy.vk01@gmail.com>
2026-02-18 17:38:33 +0700

    remove unused and add scramble for documataion api

--- app/Models/Machine.php
+++ app/Models/Machine.php
@@ -2,13 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Machine extends Model
 {
-    /** @use HasFactory<\Database\Factories\MachineFactory> */
-    use HasFactory;
 
     protected $fillable = [
         'code',
